fe7f9ed5868ee060079d813dd3399876b248db49
Enrique Thedy <ethedy@gmail.com>
2023-12-22 03:38:06 -0300

    HelloService: agregado para CLI y WEB Usamos logging e inyeccion de dependencias en CLI y WEB Agregamos proyecto para servidor nodejs

--- libreriaPTR/cli/Program.cs
+++ libreriaPTR/cli/Program.cs
@@ -1,1 +1,67 @@
 ﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Servicios;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices(serv =>
+{
+  //  serv.AddHostedService<Aplicacion2>();
+  serv.AddScoped<IHostedService, Aplicacion>();
+  serv.AddScoped<HelloService>();
+});
+
+var host = builder.Build();
+
+host.Run();
+
+
+public class Aplicacion : IHostedService
+{
+  private readonly HelloService _serv;
+
+  public Aplicacion(HelloService serv)
+  {
+    _serv = serv;
+  }
+
+  public void Ejecutar()
+  {
+    //  var hello = new HelloService();
+
+    Console.WriteLine(_serv.GetMensaje());
+  }
+
+  public Task StartAsync(CancellationToken cancellationToken)
+  {
+    Ejecutar();
+    return Task.CompletedTask;
+  }
+
+  public Task StopAsync(CancellationToken cancellationToken)
+  {
+    return Task.CompletedTask;
+  }
+}
+
+//public class Aplicacion2 : IHostedService
+//{
+//  public void Ejecutar()
+//  {
+//    var hello = new HelloService();
+
+//    Console.WriteLine(hello.GetMensaje());
+//  }
+
+//  public Task StartAsync(CancellationToken cancellationToken)
+//  {
+//    Ejecutar();
+//    return Task.CompletedTask;
+//  }
+
+//  public Task StopAsync(CancellationToken cancellationToken)
+//  {
+//    return Task.CompletedTask;
+//  }
+//}
